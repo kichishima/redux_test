303ce51f23ca1adc5f7aed8515d5b940f75be989
apex <apex@kichijimanoMac-Pro.local>
2021-07-27 10:29:23 +0900

    遷移後のページのどの階層からもstateへのアクセス可能に実装

--- pages/Page1.js
+++ pages/Page1.js
@@ -1,6 +1,7 @@
 import {useState} from 'react'
 import Link from 'next/link'
 import Counter from './components/counter'
+import ChildTwo from './components/childTwo'
 
 export default function Page1(){
     const [piyo, setpiyo] = useState("this is page1")
@@ -10,6 +11,7 @@ export default function Page1(){
             <Link href="/Page">default</Link>
             <Link href="/Page2">Page2</Link>
             <Counter/>
+            <ChildTwo/>
         </div>
     )
 }
